97301b263a41884025a52c8700abe8fbbdac843a
Vinit <vinit.2007.20@gmail.com>
2026-08-25 23:11:17 +0530

    Testing Done

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from routes.study_route import router as study_routes
 
 app = FastAPI()
 
-app.include_router(study_routes)
-
 # main page
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# testing pending !
+app.include_router(study_routes)
